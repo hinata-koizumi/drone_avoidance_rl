7744d137bc3dc6db2cb015da52593feb7f4c6019
小泉　日南太 <koizumihinata@aaaa.local>
2025-05-07 22:36:57 +0900

    Fix package.xml versions, add check_package_versions.sh, and update CI

---
+++ tests/test_gym_api.py
@@ -0,0 +1,29 @@
+import pytest
+import numpy as np
+from src.gym_env import DroneSimEnv
+
+def test_gym_api_basic():
+    env = DroneSimEnv(episode_max_steps=10)
+    obs, info = env.reset()
+    assert isinstance(obs, np.ndarray)
+    for _ in range(3):
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, info = env.step(action)
+        assert isinstance(obs, np.ndarray)
+        assert isinstance(reward, float)
+        assert isinstance(terminated, bool)
+        assert isinstance(truncated, bool)
+    env.close()
+
+def test_gym_api_render_not_implemented():
+    env = DroneSimEnv()
+    try:
+        env.render()
+    except NotImplementedError:
+        pass
+    except AttributeError:
+        pass
+    else:
+        # If render() is implemented, that's fine too
+        pass
+    env.close() 
