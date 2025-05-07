ca88b6a7c87ec37d6b49ca403e552fb2479843bc
小泉　日南太 <koizumihinata@aaaa.local>
2025-05-07 19:26:30 +0900

    Fix package.xml versions, add check_package_versions.sh, and update CI

---
+++ check_package_versions.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+echo "ROS 2パッケージのバージョン一覧を確認します..."
+
+versions=$(grep -h '<version>' src/*/package.xml | sed -E 's/.*<version>(.*)<\/version>.*/\1/' | sort | uniq)
+
+if [ "$(echo "$versions" | wc -l)" -ne 1 ]; then
+  echo "Inconsistent package.xml versions found:"
+  grep -H '<version>' src/*/package.xml
+  exit 1
+fi
+
+echo "All package.xml versions are consistent: $versions"
