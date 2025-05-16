1675150c7d014c85d3316168e3852911fcf9f355
小泉　日南太 <koizumihinata@aaaa.local>
2025-05-16 11:22:08 +0900

    Fix px4 error

--- src/sim_launch/launch/sim_all.launch.py
+++ src/sim_launch/launch/sim_all.launch.py
@@ -54,6 +54,15 @@ def generate_launch_description() -> LaunchDescription:
             output="screen",
             condition=IfCondition(LaunchConfiguration("headless"))
         ),
+        # PX4 SITL 起動
+        ExecuteProcess(
+            cmd=[
+                "px4", "-i", "0", "-d",
+                "-s", "/PX4-Autopilot/build/px4_sitl_default/etc/init.d-posix/rcS",
+                "-w", "build/px4_sitl_rtps"
+            ],
+            output="screen"
+        ),
 
         # 各ブリッジノード（ROS2 側）
         ExecuteProcess(
