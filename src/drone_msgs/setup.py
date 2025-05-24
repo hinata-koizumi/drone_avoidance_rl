efd5d2957404d1ec62df66629b3ad8c15e3a4ff4
小泉　日南太 <koizumihinata@aaaa.local>
2025-05-24 19:36:12 +0900

    Fix px4 error

---
+++ src/drone_msgs/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+package_name = 'drone_msgs'
+
+setup(
+    name=package_name,
+    version='2.0.1',
+    packages=[],
+    install_requires=[],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='user@example.com',
+    description='Custom messages for drone control and state.',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={},
+) 
