7027e9786b1453112df548e3b723d9e5c7f77ac9
小泉　日南太 <koizumihinata@aaaa.local>
2025-06-02 21:06:50 +0900

    px4ビルドエラー修正

--- docker/px4-simulator/entrypoint-px4-sitl.sh
+++ docker/px4-simulator/entrypoint-px4-sitl.sh
@@ -4,7 +4,7 @@ set -e
 MODEL=${1:-"none"}
 PARAMS=${2:-""}
 
-cd /home/px4user/PX4-Autopilot
+cd /PX4-Autopilot
 
 if [ "$MODEL" = "none" ]; then
     exec ./build/px4_sitl_default/bin/px4 -d $PARAMS
